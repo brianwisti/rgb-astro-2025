338352d4b84028494fbf9ba63e5ea9e6a95d45c5
Brian Wisti <brianwisti@pobox.com>
2024-12-24 20:10:35 -0700

    feat: cover images

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,13 @@
 import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
+import { getImage } from "astro:assets";
 
 const postSchema = z.object({
   title: z.string(),
   date: z.date(),
   tags: z.array(z.string()).default([]),
   categories: z.array(z.string()),
+  cover_image: z.string().optional(),
 });
 
 const posts = defineCollection({
