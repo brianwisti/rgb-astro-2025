fe234f5c30bcb0f30044f3deb02b32b2f068dee3
Brian Wisti <brianwisti@pobox.com>
2024-12-25 23:16:31 -0700

    fix: PCategoryLink -> TaxonomyLink for clarity

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [markdoc()],
+  integrations: [markdoc({ allowHTML: true })],
   site: "https://randomgeekery.org",
 });
