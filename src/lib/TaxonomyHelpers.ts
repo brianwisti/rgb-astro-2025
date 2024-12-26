b7b7bc98890a33ab031384a49f1c309dc1ba5f08
Brian Wisti <brianwisti@pobox.com>
2024-12-26 12:53:34 -0700

    feat: taxonomies rely on slugs

---
+++ src/lib/TaxonomyHelpers.ts
@@ -0,0 +1,62 @@
+// Functions for loading and sorting taxonomies
+
+import { getCollection } from "astro:content";
+import { default as slugify } from "slug";
+
+interface Post {
+  data: {
+    title: string
+    date: Date
+    tags: string[]
+    categories: string[]
+  };
+  id: string
+}
+
+type SluggedTaxonomyMapping = Map<string, Post[]>
+
+export async function collectSiteCategoryMap(): Promise<SluggedTaxonomyMapping> {
+  const posts = await getCollection("posts")
+  const slugs = new Map<string, Post[]>
+
+  for (let i = 0; i < posts.length; i++) {
+    const post = posts[i]
+    const categories = post.data.categories
+
+    for (let j = 1; j < categories.length; j++) {
+      const category = categories[i]
+      const slug = slugify(category)
+
+      if (!slugs.has(slug)) {
+        slugs.set(slug, [])
+      }
+
+      slugs.get(slug)?.push(post)
+    }
+  }
+
+  return slugs
+}
+
+export async function collectSiteTagMap(): Promise<SluggedTaxonomyMapping> {
+  const posts = await getCollection("posts")
+  const slugs = new Map<string, Post[]>
+
+  for (let i = 0; i < posts.length; i++) {
+    const post = posts[i]
+    const tags = post.data.tags
+
+    for (let j = 0; j < tags.length; j++) {
+      const tag = tags[j]
+      const slug = slugify(tag)
+
+      if (!slugs.has(slug)) {
+        slugs.set(slug, [])
+      }
+
+      slugs.get(slug)?.push(post)
+    }
+  }
+
+  return slugs
+}
