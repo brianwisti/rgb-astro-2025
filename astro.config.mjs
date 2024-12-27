a9fcf1bf25fcd5d383539ebb9ac40b03db4c69cb
Brian Wisti <brianwisti@pobox.com>
2024-12-27 10:47:58 -0700

    feat: MDX integration and a /follow page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 
 import markdoc from "@astrojs/markdoc";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [markdoc({ allowHTML: true })],
+  integrations: [markdoc({ allowHTML: true }), mdx()],
   site: "https://randomgeekery.org",
 });
