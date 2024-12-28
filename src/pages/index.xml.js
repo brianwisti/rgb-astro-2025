a60fa62d79c93ef79c10ec6e3e130542649e5c74
Brian Wisti <brianwisti@pobox.com>
2024-12-28 15:09:25 -0700

    feat: generator and build timestamp in RSS

--- src/pages/index.xml.js
+++ src/pages/index.xml.js
@@ -1,22 +1,34 @@
+import { DateTime } from "luxon";
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 import { getCollection } from "astro:content";
+
+function preparePost(post) {
+  return {
+    title: post.data.title,
+    description: post.data.description,
+    pubDate: post.data.date,
+    link: `https://randomgeekery.org/post/${post.id}/`,
+  }
+}
 
 export async function GET(context) {
   const allPosts = await getCollection("posts");
   const freshPosts = allPosts.sort((a, b) => {
     return new Date(b.data.date) - new Date(a.data.date)
   }).slice(0, 20);
+  const buildDate = DateTime.now().toRFC2822();
+  const customData = `
+<generator>Astro -- astro.build</generator>
+<language>en-us</language>
+<lastBuildDate>${buildDate}</lastBuildDate>
+`;
 
   return rss({
     title: "Random Geekery Blog",
-    description: "Brian Wisti's blog",
+    description: "Recent blog entries on Random Geekery",
     site: context.site,
-    items: freshPosts.map((post) => ({
-      title: post.data.title,
-      description: post.data.title,
-      pubDate: post.data.date,
-      link: `https://randomgeekery.org/post/${post.id}/`,
-    })),
-    customData: `<language>en-us</language</language>`,
+    lastBuildDate: buildDate,
+    items: freshPosts.map((post) => preparePost(post)),
+    customData: customData,
   });
 }
