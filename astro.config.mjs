3b3e2c80144417d7b8384c836cf8a5c438fa75d8
Brian Wisti <brianwisti@pobox.com>
2024-12-30 14:45:27 -0700

    feat: remark supports directives and definition lists

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,37 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import markdoc from "@astrojs/markdoc";
+import astroExpressiveCode from "astro-expressive-code"
+import markdoc from "@astrojs/markdoc"
+import mdx from "@astrojs/mdx"
+import mdxDirective from "astro-mdx-directive"
+import {
+  remarkDefinitionList,
+  defListHastHandlers,
+} from "remark-definition-list"
 
-import mdx from "@astrojs/mdx";
-
-import astroExpressiveCode from "astro-expressive-code";
+/** @type {import("astro-mdx-directive").DirectiveComponentList} */
+const directives = {
+  container: [
+    {
+      name: "note",
+      path: "src/components/Note.astro",
+    },
+  ],
+  leaf: [],
+  text: [],
+};
 
 export default defineConfig({
   integrations: [
     astroExpressiveCode(),
     markdoc({ allowHTML: true }),
+    mdxDirective({ directives }),
     mdx()
   ],
+  markdown: {
+    remarkPlugins: [remarkDefinitionList],
+    remarkRehype: { handlers: defListHastHandlers },
+  },
   site: "https://randomgeekery.org",
-});
+})
