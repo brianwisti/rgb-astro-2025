96c2dbb03a8a227868ae01420ea41f5f77500eed
Brian Wisti <brianwisti@pobox.com>
2025-01-01 15:09:33 -0700

    test: check Markdown structure and tag count

--- tests/sources.test.ts
+++ tests/sources.test.ts
@@ -1,6 +1,8 @@
 import fs from "node:fs"
 import glob from "fast-glob"
-import * as matter from "gray-matter";
+import * as matter from "gray-matter"
+import { load } from "js-yaml"
+import { lint, readConfig } from "markdownlint/sync"
 import { describe, expect, test } from "vitest"
 
 import { PostSchema } from "@lib/Post.ts"
@@ -10,29 +12,38 @@ type ParsedPost = {
   post: matter.GrayMatterFile<string>
 }
 
+const tagLimit = 3
+
 const posts: ParsedPost[] = glob.sync("src/posts/**/*.{mdx,mdoc}")
   .map((path: string) => {
     return { path: path, post: matter.default(fs.readFileSync(path, "utf8")) }
   }
 )
 
-
-describe("Content", () => {
-  test("every post is accounted for", () => {
-    const postFileCount = glob.sync("src/posts/**/*.{mdx,mdoc}").length
-    console.log("postFileCount", postFileCount)
-
-    expect(posts.length).toBeGreaterThan(0)
-    expect(posts.length).toBe(postFileCount)
-  })
-})
+const markdownlintConfig = readConfig(".markdownlint.yml", [load])
 
 describe.each(posts)("$path", ({ path, post}) => {
   test("frontmatter is valid PostSchema", () => {
     const validation = PostSchema.safeParse(post.data)
 
-    // expect(validation.success).toBe(true)
     expect(validation.error).toBeUndefined()
+  })
+
+  test("content is consistent Markdown", () => {
+    const { content } = post
+    const result = lint({
+      strings: { content },
+      config: markdownlintConfig,
+    })
+
+    expect(result.toString()).toBe("")
+  })
+
+  test("not too many tags", () => {
+    const tags = post.data.tags
+
+    expect(tags).toBeInstanceOf(Array)
+    expect(tags.length).toBeLessThanOrEqual(tagLimit)
   })
 })
 
