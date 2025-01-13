44bdb1f1cd31d56b2e764c94d34e0ab01ae35093
Brian Wisti <brianwisti@pobox.com>
2025-01-13 12:14:15 -0700

    feat: simple POSSE links

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,20 @@
 import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
+import { z } from "astro:content"
 
-import { PostSchema } from "@lib/Post";
+export const PostSchema = z.object({
+  title: z.string(),
+  date: z.date(),
+  categories: z.array(z.string()),
+  tags: z.array(z.string()).default([]),
+  uses: z.array(z.string()).default([]),
+  series: z.array(z.string()).default([]),
+  aliases: z.array(z.string()).default([]),
+  description: z.string().optional(),
+  cover_caption: z.string().default(""),
+  cover_image: z.string().optional(),
+  posse: z.array(z.string()).default([]),
+})
 
 const posts = defineCollection({
   loader: glob({ pattern: "./**/[^_]*.{mdx,mdoc}", base: "src/posts" }),
--- src/lib/Post.ts
+++ src/lib/Post.ts
@@ -1,29 +1,18 @@
-import { z } from "astro:content"
-
-export const PostSchema = z.object({
-  title: z.string(),
-  date: z.date(),
-  categories: z.array(z.string()),
-  tags: z.array(z.string()).default([]),
-  uses: z.array(z.string()).default([]),
-  series: z.array(z.string()).default([]),
-  aliases: z.array(z.string()).default([]),
-  description: z.string().optional(),
-  cover_caption: z.string().default(""),
-  cover_image: z.string().optional(),
-})
-
-export interface PostData {
+export interface PageData {
   title: string
-  date: Date
   description?: string
+  cover_image?: string
+  cover_caption?: string
+  aliases?: string[]
+  posse?: string[]
+}
+
+export interface PostData extends PageData {
+  date: Date
   tags: string[]
   categories: string[]
   uses: string[]
   series: string[]
-  cover_image?: string
-  cover_caption?: string
-  aliases?: string[]
 }
 
 export interface PostInterface {
