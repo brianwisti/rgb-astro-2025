b6a23c4c49387a3ca8cfb252db4a406245ef8245
Brian Wisti <brianwisti@pobox.com>
2025-01-18 20:57:51 -0700

    feat: 404 page

--- src/pages/htaccess.js
+++ src/pages/htaccess.js
@@ -3,9 +3,15 @@ import { getCollection } from "astro:content"
 export async function GET(context) {
   const posts = await getCollection("posts")
 
-  const directiveText = posts.map((post) =>
+  const htaccessRules = [
+    "ErrorDocument 404 /404.html",
+  ]
+
+  const redirectRules = posts.map((post) =>
     post.data.aliases.map((alias) => `Redirect 301 ${alias} /post/${post.id}/`)
-  ).flat().join("\n")
+  ).flat()
+
+  const directiveText = htaccessRules.concat(redirectRules).join("\n")
 
   return new Response(directiveText)
 }
